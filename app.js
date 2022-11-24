9794d84dd0072c0c5cebb5256f493af5cce9e50d
Dada Clement <dadaauthourity23@gmail.com>
2022-11-24 10:51:17 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// require packages
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -13,77 +14,26 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.use(session({ /////for session creation needed for authentication.
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-mongoose.connect("mongodb://localhost:27017/socialDB");
-
-const userSchema = new mongoose.Schema({
-    username: String,
-    password:  String,
+/////routing
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
 
-userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
 
 
-const messageSchema = {
-    message: String
-};
 
-const User = mongoose.model("User", userSchema);
-passport.use(User.createStrategy());
+ 
 
-passport.serializeUser(function(user, cb) {
-    process.nextTick(function() {
-            return cb(null, {
-        id: user.id,
-        username: user.username,
-      });
-      
-    });
-  });
+
+
+
+
+
+
+                                            ///////Register and Login ////////////
+
   
-  passport.deserializeUser(function(user, cb) {
-    process.nextTick(function() {
-      return cb(null, user);
-    });
-  });
-
-const Message = mongoose.model("Message", messageSchema);
-
-
-app.get("/message", (req, res) => {
-    Message.find({}, (err, found) => {
-         res.render("message", {messages: found});
-    });
-
-});
-
-app.post("/message", (req, res) => {
-    const message = new Message({
-        message: req.body.message
-    });
-    message.save();
-    res.redirect("/");
-});
-
-app.get("/deleteMessage/:messageId", (req, res) => {
-    Message.findByIdAndDelete({_id: req.params.messageId}, (err, done) => {
-        if(err){
-            res.send(err);
-        } else if (done){
-            res.redirect("/message");
-        }
-    });
-});
+  
 
 app.listen(3000, () => {console.log("app running on port 3000");});
